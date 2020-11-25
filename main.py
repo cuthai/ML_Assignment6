36fb147cd19fe121aefbdb09f575d47c95ef3fca
Christopher Thai <christopheruthai@gmail.com>
2020-11-24 17:49:32 -0800

    Added structure

---
+++ main.py
@@ -0,0 +1,18 @@
+from utils.args import args
+from racetrack.board import Track
+
+
+def main():
+    """
+    Main function to run Neural Network
+    """
+    # Parse arguments
+    arguments = args()
+
+    board = Track()
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
